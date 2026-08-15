ec277bb39c9d03d050eebd94ce7d1883fbe03ef4
Devotion <zcysirr@gmail.com>
2026-08-16 00:58:49 +0800

    feat: register backup restore commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,15 @@
+mod backup;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![
+            backup::create_database_backup,
+            backup::restore_database_backup
+        ])
         .run(tauri::generate_context!())
         .expect("error while running KylinStock");
 }
